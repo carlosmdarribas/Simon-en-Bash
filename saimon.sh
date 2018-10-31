3da97433384bd3c3269ce84712c8d49e179f560e
carlosmdarribas <carlosmdarribas@gmail.com>
2018-10-31 19:34:08 +0100

    Correccion de errores Implementación parametros para ver autores Implementación del menu, con colores Implementacion método de control de errores numérico Implementación método press to continue Implementacion menu principal con opciones Implementacion opciones salir

--- saimon.sh
+++ saimon.sh
@@ -1,23 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 #  saimon.sh
 #  TrabajoSSOOI_I
 #
 #  Created by Güe Both on 31/10/18.
-#  
+#
 
-#! /bin/bash
-
-
-
-function TEST_ARGUMENTS {
+function TEST_ARGUMENTS
+{
     if [[ $# -lt 2 ]]; then
         if [[ $# -eq 1 ]]; then
             if [[ $1 = "-g"  ]]; then
                 SHOW_GROUP_DATA;
+                SALIR=true
             else
                 ERROR=2
-                ALLOWED_ARGUMENTS
+                ALLOWED_ARGUMENTS  #IMPLEMENT
             fi
         else
             EXECUTE_GAME;
@@ -27,20 +25,106 @@ function TEST_ARGUMENTS {
     fi
 }
 
-function SHOW_GROUP_DATA {
+function SHOW_GROUP_DATA
+{
     echo "Er Gonza y er Carlo han hesho eto" # DEBUG: Modificar en producción
 }
 
-function SHOW_MENU {
-    echo " J)JUGAR\nC)CONFIGURACIÓN\nE)ESTADÍSTICAS\nS)SALIR\n“Saimon”. Introduzca una opción >>"
+function CHECK_ERROR
+{
+    if [[ $ERROR -ne 0 ]]; then
+        PRINT_ERROR ERROR  #IMPLEMENTAR PRINT_ERROR
+    fi
+}
+
+
+function DISPLAY_MENU
+{
+    echo -ne "\033]11;#800000\007"
+
+    GRAY='\e[95m'
+
+    RED='\033[0;31m'  # DEBUG: Pasar a constantes
+    GREEN='\033[0;32m'  # DEBUG: Pasar a constantes
+    BLUE='\033[0;36m'  # DEBUG: Pasar a constantes
+    YELLOW='\033[0;33m'  # DEBUG: Pasar a constantes
+    NC='\033[0m' # No Color
+
+    echo -e "\n${GRAY}"
+    echo "███████╗ █████╗ ██╗███╗   ███╗ ██████╗ ███╗   ██╗"
+    echo "██╔════╝██╔══██╗██║████╗ ████║██╔═══██╗████╗  ██║"
+    echo "███████╗███████║██║██╔████╔██║██║   ██║██╔██╗ ██║"
+    echo "╚════██║██╔══██║██║██║╚██╔╝██║██║   ██║██║╚██╗██║"
+    echo "███████║██║  ██║██║██║ ╚═╝ ██║╚██████╔╝██║ ╚████║"
+    echo "╚══════╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝"
+    echo -e "${NC}\n"
+
+    echo -e "${RED}J) JUGAR"
+    echo -e "${GREEN}C) CONFIGURACIÓN"
+    echo -e "${BLUE}E) ESTADÍSTICAS"
+    echo -e "${YELLOW}S) SALIR"
+    echo -e "${NC}"
+
+}
+
+function PRESS_TO_CONTINUE
+{
+    echo -e "\nPulse <INTRO> para continuar."
+    read
+    echo -e "\n"
 }
 ###########################################################################################################################################################
 ###########################################################################################################################################################
-#                                                     +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+                                                                      #
-#                                                     |F|U|N|C|I|O|N| |P|R|I|N|C|I|P|A|L|                                                                      #
-#                                                     +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+                                                                      #
+#                                                     +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+                                                                 #
+#                                                     |F|U|N|C|I|O|N| |P|R|I|N|C|I|P|A|L|                                                                 #
+#                                                     +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+                                                                 #
 #=========================================================================================================================================================#
 
 ERROR=0 #0 = No errors
+SALIR=false
 
 TEST_ARGUMENTS $1 $2
+
+CHECK_ERROR ERROR
+
+until test $SALIR = true
+    do
+        clear
+        DISPLAY_MENU
+        printf "\nSeleccione una opcion: "
+        read OPTION
+
+        case $OPTION in
+            "J")
+                clear
+                GAME
+                PRESS_TO_CONTINUE
+                ;;
+            "C")
+                clear
+                CONFIG_MENU
+                PRESS_TO_CONTINUE
+                ;;
+            "E")
+                clear
+                STATS
+                PRESS_TO_CONTINUE
+                ;;
+            "S")
+                SALIR=true
+                ;;
+            *)
+                echo -e"\nOpción Incorrecta."
+                PRESS_TO_CONTINUE
+                ;;
+        esac
+    done
+
+
+
+
+
+
+
+
+
